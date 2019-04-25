9ab2f01552e3a5e39fff2bd554b77007f5f613d6
Dylan McClain <dylan.mcclain@gmail.com>
2019-04-24 17:55:48 -0700

    add some nested routes

--- src/App.js
+++ src/App.js
@@ -14,6 +14,34 @@ function AddFlower() {
   return <h2>Add Flower</h2>
 }
 
+function ShowFlower({ match }) {
+  return <h3>Requested Param: {match.params.id}</h3>
+}
+
+function ShowFlowers({ match }) {
+  return (
+    <div>
+      <h2>Show Flower & Sightings</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/add-sighting`}>Add Sighting</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/edit`}>Edit Flower</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/delete`}>Delete Flower</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:id`} component={ShowFlower} />
+      <Route exact path={match.path} render={() => <h3>Please select an option.</h3>} />
+    </div>
+  )
+}
+
+
 function App() {
   return (
     <Router>
@@ -27,6 +55,9 @@ function App() {
               <Link to="/index/">Flowers</Link>
             </li>
             <li>
+              <Link to="/show/">Show Flower</Link>
+            </li>
+            <li>
               <Link to="/new/">Add Flower</Link>
             </li>
           </ul>
@@ -34,6 +65,7 @@ function App() {
 
         <Route exact path="/" component={Home} />
         <Route path="/index/" component={Flowers} />
+        <Route path="/show/" component={ShowFlowers} />
         <Route path="/new/" component={AddFlower} />
       </div>
     </Router>
