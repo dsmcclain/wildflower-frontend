1371129b21b67961a6b7742177b28ac272e1d75c
Dylan McClain <dylan.mcclain@gmail.com>
2019-04-24 17:44:36 -0700

    remove bloat, create basic routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Flowers() {
+  return <h2>Flowers</h2>
+}
+
+function AddFlower() {
+  return <h2>Add Flower</h2>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/index/">Flowers</Link>
+            </li>
+            <li>
+              <Link to="/new/">Add Flower</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route exact path="/" component={Home} />
+        <Route path="/index/" component={Flowers} />
+        <Route path="/new/" component={AddFlower} />
+      </div>
+    </Router>
   );
 }
 
